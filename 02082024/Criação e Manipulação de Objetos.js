58035284c3ed0943dac068fb02c71f1c29a42af8
JoaoGuilhermeVaz <162444627+JoaoGuilhermeVaz@users.noreply.github.com>
2024-08-02 09:00:42 -0300

    Add files via upload

---
+++ 02082024/Criação e Manipulação de Objetos.js
@@ -0,0 +1,13 @@
+let carro = {
+    marca: 'cytroën',
+    modelo: 'aircross',
+    ano: 2012,
+   
+detalhes: function() {
+       console.log(`Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}`);
+    }
+};
+carro.detalhes();
+carro.cor = 'Branco';
+console.log(carro);
+
---
+++ 02082024/exemplo.js
@@ -0,0 +1,34 @@
+let primeironome = "nome"
+let ultimonome = "sobrenome"
+let nomecompleto = primeironome + " "
+console.log(nomecompleto)
+
+let numero1 = '10'
+let numero2 = 20
+
+let soma = numero1 + numero2
+let subtração = numero1 - numero2
+let multiplicação = numero1 * numero2
+let divisão = numero1 ? numero2
+
+console.log(soma + '\n' + subtração + '\n' + multiplicação + '\n' + divisão)
+
+let estachovendo = false
+if(estachovendo){
+    console.log("está chovendo!")
+}else{
+    console.log("não está chovendo!")
+}
+
+let resposta = null
+let nota
+
+console.log("resposta =" + resposta)
+console.log("nota = "+ nota)
+
+let idnico = symbol("id")
+let produto ={
+    {idunico}: 123
+}
+
+console.log(produto{idunico})
